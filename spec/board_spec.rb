8ca2213d54ff8d9db29acebb7606d46a1ff10232
yokn <yokn@protonmail.com>
2020-10-07 21:55:21 +0300

    add tests for pawn special moves

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -33,13 +33,13 @@ describe Board do
       subject(:board) { described_class.new }
       before do
         expect(board).to receive(:player_input).and_return('a2')
-        expect(board).to receive(:player_input).and_return('a4')
+        expect(board).to receive(:player_input).and_return('a3')
         board.get_move('W')
       end
       context 'the move is valid' do
         it 'adds the move to the board' do
           board_array = board.instance_variable_get(:@board)
-          tile = board_array[4][0]
+          tile = board_array[5][0]
           expect(tile.class.name).to eq('Pawn')
           expect(tile.color).to eq('W')
         end
@@ -48,8 +48,8 @@ describe Board do
                                     'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P',
                                     '-', '-', '-', '-', '-', '-', '-', '-',
                                     '-', '-', '-', '-', '-', '-', '-', '-',
-                                    'P', '-', '-', '-', '-', '-', '-', '-',
                                     '-', '-', '-', '-', '-', '-', '-', '-',
+                                    'P', '-', '-', '-', '-', '-', '-', '-',
                                     '-', 'P', 'P', 'P', 'P', 'P', 'P', 'P',
                                     'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'])
         end
@@ -67,6 +67,80 @@ describe Board do
           expect(target_tile1).to eq('-')
           expect(target_tile2).to eq('-')
         end
+      end
+    end
+    context 'movement' do
+      subject(:board) { described_class.new }
+      it 'pawns can double move if they are at their starting positions' do
+        expect(board).to receive(:player_input).and_return('a2')
+        expect(board).to receive(:player_input).and_return('a4')
+        board.get_move('W')
+        board_array = board.instance_variable_get(:@board)
+        tile = board_array[4][0]
+        expect(tile.class.name).to eq('Pawn')
+        expect(tile.color).to eq('W')
+      end
+      it 'pawns cannot double move if they are not at their starting positions' do
+        # setup to move piece away from its starting position
+        expect(board).to receive(:player_input).and_return('a2')
+        expect(board).to receive(:player_input).and_return('a3')
+        board.get_move('W')
+
+        expect(board).to receive(:player_input).and_return('a3')
+        expect(board).to receive(:player_input).and_return('a5')
+        expect(board).to receive(:invalid_move)
+        board.get_move('W')
+        board_array = board.instance_variable_get(:@board)
+        own_tile = board_array[5][0]
+        target_tile = board_array[3][0]
+        expect(own_tile.class.name).to eq('Pawn')
+        expect(target_tile).to eq('-')
+      end
+      it 'pawns can be blocked' do
+        # white and black pawns are against each other on the A file
+        expect(board).to receive(:player_input).and_return('a2')
+        expect(board).to receive(:player_input).and_return('a4')
+        board.get_move('W')
+        expect(board).to receive(:player_input).and_return('a7')
+        expect(board).to receive(:player_input).and_return('a5')
+        board.get_move('B')
+
+        # white pawn cannot move forward because it is blocked
+        expect(board).to receive(:player_input).and_return('a4')
+        expect(board).to receive(:player_input).and_return('a5')
+        expect(board).to receive(:invalid_move)
+        board.get_move('W')
+        # black pawn cannot move forward because it is blocked
+        expect(board).to receive(:player_input).and_return('a5')
+        expect(board).to receive(:player_input).and_return('a4')
+        expect(board).to receive(:invalid_move)
+        board.get_move('B')
+
+        board_array = board.instance_variable_get(:@board)
+        white_tile = board_array[4][0]
+        black_tile = board_array[3][0]
+        expect(white_tile.class.name).to eq('Pawn')
+        expect(white_tile.color).to eq('W')
+        expect(black_tile.class.name).to eq('Pawn')
+        expect(black_tile.color).to eq('B')
+      end
+      it 'pawns can capture diagonally' do
+        expect(board).to receive(:player_input).and_return('a2')
+        expect(board).to receive(:player_input).and_return('a4')
+        board.get_move('W')
+        expect(board).to receive(:player_input).and_return('b7')
+        expect(board).to receive(:player_input).and_return('b5')
+        board.get_move('B')
+        expect(board).to receive(:player_input).and_return('a4')
+        expect(board).to receive(:player_input).and_return('b5')
+        board.get_move('W')
+
+        board_array = board.instance_variable_get(:@board)
+        own_tile = board_array[4][0]
+        target_tile = board_array[3][1]
+        expect(target_tile.class.name).to eq('Pawn')
+        expect(target_tile.color).to eq('W')
+        expect(own_tile).to eq('-')
       end
     end
     # context 'when checking for a win' do
